f52b893da7f5220ba852568a911a58d9021c07a9
ABDULLAH ALAJLAN <abdullahalajlan@ABDULLAHs-MBP.lan>
2026-03-10 09:50:13 +0300

    Update ALLOWED_HOSTS to allow railway subdomains

--- property_management/settings.py
+++ property_management/settings.py
@@ -15,7 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+import os
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,.railway.app').split(',')
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1:8000').split(',')
 
 
