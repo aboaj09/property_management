8770ea6e21879d41ceee01dd9d43a2262d20bdf1
ABDULLAH ALAJLAN <abdullahalajlan@ABDULLAHs-MBP.lan>
2026-03-10 18:29:46 +0300

    Add new domain to ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- property_management/settings.py
+++ property_management/settings.py
@@ -18,7 +18,9 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 import os
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,.railway.app').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1:8000').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS','https://amar.up.railway.app',
+    'https://*.up.railway.app','http://127.0.0.1:8000').split(',')
+
 
 
 # Application definition
