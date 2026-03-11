4a644a3bba4232e37f1fcb86bdba59f7f1f24202
ABDULLAH ALAJLAN <abdullahalajlan@ABDULLAHs-MBP.lan>
2026-03-11 22:01:18 +0300

    Remove unique constraint on identity_number, rely on code check

--- rentals/models.py
+++ rentals/models.py
@@ -111,8 +111,6 @@ class Tenant(models.Model):
         verbose_name = "مستأجر"
         verbose_name_plural = "المستأجرون"
         ordering = ['name']
-        unique_together = ['identity_number', 'is_deleted']  # يسمح بتكرار الرقم مع is_deleted=True
-
     def __str__(self):
         return f"{self.name} - {self.identity_number}"
 
